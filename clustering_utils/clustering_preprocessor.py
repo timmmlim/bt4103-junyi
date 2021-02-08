764384dd082598b7bd0dceeb7fef38e3ad43a9ae
kianyewn <kianyew.n@gmail.com>
2021-02-08 21:23:07 +0800

    updated changes to pipeline

--- clustering_utils/clustering_preprocessor.py
+++ clustering_utils/clustering_preprocessor.py
@@ -7,33 +7,72 @@ from sklearn.preprocessing import StandardScaler
 
 class bt4103Preprocessor:
     def __init__(self, data):
+        self.data = data# data
+        self.pca = None
+        self.columns = data #data.columns
+        
+    def scale_numerical_data(self, data):
         self.data = data
-
-    def scale_numerical_data(self):
+        self.columns = data.columns
         sc = StandardScaler()
-        sc.fit(self.data)
-        transformed_data = sc.transform(self.data)
+        sc.fit(data)
+        transformed_data = sc.transform(data)
         self.transformed_data = transformed_data
-        return transformed_data
+        return self.transformed_data
 
     def perform_pca(self, n_components=None, seed=4103):
-        pca = PCA(n_components, whiten=True, random_state=seed)
+        
+        pca = PCA(n_components=n_components, whiten=True, random_state=seed)
+        self.data = self.scale_numerical_data(self.data)
         pca.fit(self.data)
         self.pca_data = pca.transform(self.data)
         self.pca = pca
         return self.pca_data
 
+    def plot_pca_result(self, ax=None, n_components=None, seed=4103):
+        if self.pca is None:
+            self.perform_pca(n_components,seed)
+            
+        components = pd.DataFrame(
+            np.round(self.pca.components_, 4), columns=self.columns)
+        components.index = [f'D{i}' for i in range(
+            1, len(self.pca.components_)+1)]
+        ratios = self.pca.explained_variance_ratio_.reshape(-1, 1)
+        variance_ratios = pd.DataFrame(
+            np.round(ratios, 4), columns=['Explained Variance'])
+        variance_ratios.index = components.index
+        
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(14, 8))
+            # Plot the feature weights as a function of the components
+            components.plot(ax=ax, kind='bar')
+            ax.set_ylabel('Feature Weights')
+            ax.set_xticklabels(components.index, rotation=0)
+            for i, ev in enumerate(self.pca.explained_variance_ratio_):
+                ax.text(i-0.40, ax.get_ylim()[1] + 0.05,
+                           "%.4f" % (ev))
+            plt.legend(title='Features', bbox_to_anchor=(1.05, 1))
+            plt.tight_layout()
+        else:
+            components.plot(ax=ax, kind='bar')
+            ax.set_ylabel('Feature Weights')
+            ax.set_xticklabels(components.index, rotation=0)
+            for i, ev in enumerate(self.pca.explained_variance_ratio_):
+                ax.text(i-0.40, ax.get_ylim()[1] + 0.05,
+                           "%.4f" % (ev))
+      
+        
     def pca_result(self):
         components = pd.DataFrame(
-            np.round(self.pca.components_, 4), columns=self.data.keys())
-        components.index = [f'Dimension_{i}' for i in range(
+            np.round(self.pca.components_, 4), columns=self.columns)
+        components.index = [f'D{i}' for i in range(
             1, len(self.pca.components_)+1)]
         ratios = self.pca.explained_variance_ratio_.reshape(-1, 1)
         variance_ratios = pd.DataFrame(
             np.round(ratios, 4), columns=['Explained Variance'])
         variance_ratios.index = components.index
 
-        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(14, 8))
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(14, 4))
         # Plot the feature weights as a function of the components
         components.plot(ax=ax[0], kind='bar')
         ax[0].set_ylabel('Feature Weights')
@@ -41,7 +80,7 @@ class bt4103Preprocessor:
 
         for i, ev in enumerate(self.pca.explained_variance_ratio_):
             ax[0].text(i-0.40, ax[0].get_ylim()[1] + 0.05,
-                       "Explained Variance\n       %.4f" % (ev))
+                       "%.4f" % (ev))
 
             ax[1].plot(np.cumsum(self.pca.explained_variance_ratio_))
             ax[1].set_xlabel('number of components')
